fcb3ab174ca80e4b3653307f5de30bf5f368d14d
hexvessel <dho1@hi.is>
2025-01-14 23:29:19 +0000

    rebbi

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import {
   ReactElement,
   SetStateAction,
+  forwardRef,
   useEffect,
   useRef,
   useState,
@@ -17,14 +18,26 @@ function App() {
     { filename: "empty", contents: "hey", bookmarks: [] },
   ]);
 
+  const rebbi = useRef(null);
+  const clickety = (e: any) => {
+    e.preventDefault();
+    // @ts-ignore
+    rebbi.current?.scrollIntoView({ behavior: "smooth" });
+  };
+  const Target = forwardRef((props, ref) => {
+    return (
+      //@ts-ignore
+      <div ref={ref}></div>
+    );
+  });
   const toggleEditing = () => {
-    setEditing(!editing);
+    //setEditing(!editing);
   };
   const getFiles = async () => {
     // @ts-ignore
     const mdFilenames = await window.electron.getMarkdownFiles();
     setFiles(mdFilenames);
-    parser(files[0].contents);
+    //parser(files[0].contents);
   };
   useEffect(() => {
     getFiles();
@@ -43,7 +56,7 @@ function App() {
           title
         </Col>
         <Col sm={1}>
-          <Button onClick={toggleEditing}>Edit</Button>
+          <Button onClick={clickety}>Edit</Button>
         </Col>
       </Row>
       <Row className="mb-3">
@@ -58,7 +71,54 @@ function App() {
           {editing ? (
             files[selected].contents
           ) : (
-            <Markdown>{files[selected].contents}</Markdown>
+            <div>
+              <Markdown># Vocant ictu tuearis</Markdown>
+              <Markdown> ## Adeste animos inpia</Markdown>
+              <Markdown>
+                cupido manibus vocalia non Lorem markdownum dictu temerarius
+                venisses te parvo faciat inhibente sperneret suorum. Qui acui
+                cristis pennas a quoque [cum praebere](http://www.creatis.org/)
+                quae, ab. Supplex natus. ## Et equo ab Nelei territus liquores
+                imagine Essem tardus animum conlapsa dorso auro, dei alimenta
+                ignoscere veneni? Vidit corporeusque sidera, partem, geniti opem
+                silvas _carmine_. Et nymphae nitebat figuras _corpus
+                nebulasque_, promissaque quae, quo vox adest cernunt: quas iam
+                muneris. Urbes concordia? \n ## Sustinet latuit concursibus
+                eductam Adunco Medusae \n ego salices relinquit et furit
+                submovet instar, socium palustres infelix Exigit.
+                [Adsiduis](http://www.concutiuntque.com/cavo.html) dona [advena
+                tum](http://adspergine.io/furialibus-pietas.html) quae numerare
+                boum, tantum aequa, ac causa? Amnes sole tenuisse nati iam
+                trepidoque, temptemus
+                [filius](http://utrimque.io/ruit-aphareus), ut mente potuit hoc:
+                Athenae. [Proxima](http://locumque.io/) oculos inque Hylen sine
+                rotatum iunxit in sed dum patiuntur? Mactare quoque tempora
+                argenteus; fassaque favorque ut nuper flamma tot ostro
+                sequuntur.\n
+              </Markdown>
+              <Markdown>
+                ## Et dives et matrumque mihi tollit perspicit \n
+              </Markdown>
+
+              <Markdown>
+                Non quis quam Ismenides potui procellamnos Aeson moratus victrix
+                cognoscendo saepe superantque! Nomen unum; me nunc nitidum: esse
+                sidera nactus potest tenet. Ipse que penetralia viginti
+                discedere flammis feroces vetat, o coniugis pennis! Ego in est
+                procul levabas ex ulla excitus vincula mihi eundo fugacia
+                saltem, Phoebus caedis. Dare inlimis denique omen retentus teli?
+                Tum inane aetatis plenaque, tibi habet cruoris Aetna, laetos num
+                agat eramque. Micant per et sua mortisque dictis commissaque ego
+                tellus: pater fractarum respiceret extis manus. Germanae
+                meliore, quae alii, credas silvis fit latis miserata. Et
+                inclamare ingreditur corpus scelus, una nisi onus rerum.
+                Detrahat aqua _me sedem perspicit_ herbas: _mens cum quacumque_
+                orbem tuentem memorque in cognosceret cura profugi! Bucina tua
+                caeli et regia.
+              </Markdown>
+
+              <Target ref={rebbi} />
+            </div>
           )}
         </Col>
       </Row>
@@ -93,9 +153,12 @@ function parser(text: string) {
   let i = 0;
   const lines = text.split("\n");
   let bookmarks: string[] = [];
+  let jsxString = "";
   while (i < lines.length) {
-    if (lines[i].startsWith("#")) {
-      console.log(lines[i]);
+    if (lines[i].startsWith("# ")) {
+      jsxString += `<div href=>`;
+    } else if (lines[i].startsWith("## ")) {
+    } else if (lines[i].startsWith("### ")) {
     }
     i++;
   }
